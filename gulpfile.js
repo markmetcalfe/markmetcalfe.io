300dd9590d02b38c5b724359a0b68a0c9bc9d03a
Mark Metcalfe <mark.ls.metcalfe@gmail.com>
2020-09-14 17:54:31 +1200

    Update gulp and packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
-var fs = require('fs');
 var path = require('path');
-var del = require('del');
 var gulp = require('gulp');
 var browserify = require('browserify');
 var injectSvg = require('gulp-inject-svg');
@@ -12,33 +10,18 @@ var $$ = require('gulp-load-plugins')();
 var autoprefixer = require('autoprefixer');
 var reload = browserSync.reload;
 var through2 = require('through2');
-var htmlclean = require('gulp-htmlclean');
 var replace = require('gulp-token-replace');
 var config = require('./variables.json');
-
-function handleError(err) {
-  console.log(err.toString());
-  this.emit('end');
-}
 
 var assetFolder = 'assets/';
 var devFolder = 'src/';
 var buildFolder = 'build/';
-var toMove = buildFolder + 'index/*';
-
-function getFolders(dir) {
-  return fs.readdirSync(dir)
-  .filter(function(file) {
-    return fs.statSync(path.join(dir, file)).isDirectory();
-  });
-}
 
 gulp.task('styles', function() {
   return gulp.src(devFolder + '**/*.scss')
   .pipe($$.sass())
   .on('error', $$.notify.onError("Error: <%= error.message %>"))
-  .on('error', handleError)
-  .pipe($$.postcss([ autoprefixer({ browsers: ["> 0%"] }) ]))
+  .pipe($$.postcss([ autoprefixer() ]))
   .pipe(gulp.dest(buildFolder))
   .pipe($$.minifyCss({keepSpecialComments: 1}))
   .pipe(gulp.dest(buildFolder))
@@ -46,60 +29,47 @@ gulp.task('styles', function() {
 });
 
 gulp.task('scripts', function() {
-  var folders = getFolders(devFolder);
-  if (folders.length === 0) return done();
-  return folders.map(function(folder) {
-    return gulp.src(path.join(devFolder, folder, '/**/*.js'))
-    .pipe(through2.obj(function (file, enc, next){
-      browserify(file.path)
-      .transform(babelify)
-      .bundle(function(err, res){
-        file.contents = res;
-        next(null, file);
+  return gulp.src(path.join(devFolder, '/*.js'))
+    .pipe(
+      through2.obj(function (file, enc, next) {
+        browserify(file.path)
+          .transform(babelify)
+          .bundle(function(err, res){
+            file.contents = res;
+            next(null, file);
+          })
       })
-    }))
+    )
     .pipe(buffer())
     .pipe(uglify.apply())
     .pipe($$.sourcemaps.write('./'))
     .pipe($$.rename('bundle.js'))
-    .pipe(gulp.dest(buildFolder + folder))
+    .pipe(gulp.dest(buildFolder))
     .pipe(reload({ stream:true }));
-  });
 });
 
 gulp.task('pages', function() {
   return gulp.src(devFolder + '**/*.html')
-  .pipe(buffer())
-  .pipe(replace({global:config}))
-  .pipe(injectSvg({
-    base: '/assets/'
-  }))
-  .pipe(gulp.dest(buildFolder))
-	.pipe(reload({ stream:true }));
+    .pipe(buffer())
+    .pipe(replace({global:config}))
+    .pipe(injectSvg({
+      base: '/assets/'
+    }))
+    .pipe(gulp.dest(buildFolder))
+    .pipe(reload({ stream:true }));
 });
 
 gulp.task('assets', function() {
   return gulp.src(assetFolder + '**/*')
-  .pipe(gulp.dest(buildFolder))
-	.pipe(reload({ stream:true }));
+    .pipe(gulp.dest(buildFolder))
+    .pipe(reload({ stream:true }));
 });
 
-gulp.task('moveindex', ['styles', 'scripts', 'pages'], function() {
-  return gulp.src(toMove)
-  .pipe(gulp.dest(buildFolder))
-});
+gulp.task('build', gulp.parallel('styles', 'scripts', 'pages', 'assets'));
 
-gulp.task('clean', ['styles', 'scripts', 'pages', 'moveindex'], function() {
-  fs.rmdir(toMove, err => {
-    return del(toMove, {force:true});
-  });
-});
-
-gulp.task('build', ['styles', 'scripts', 'assets', 'pages', 'moveindex', 'clean']);
-
-gulp.task('serve', ['build'], function() {
+gulp.task('serve', gulp.series('build', function() {
   browserSync({ server: { baseDir: buildFolder } });
-  gulp.watch(devFolder + '**/*', ['build'], reload);
-});
+  gulp.watch(devFolder + '**/*', gulp.series('build'), reload);
+}));
 
-gulp.task('default', ['build']);
+gulp.task('default', gulp.series('build'));
