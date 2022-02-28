49fc661cd98c0bfe942eb19e538ab3a0449c0d20
Mark Metcalfe <mark.ls.metcalfe@gmail.com>
2022-02-28 14:38:39 +1300

    Remove fontawesome js import to improve performance

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
