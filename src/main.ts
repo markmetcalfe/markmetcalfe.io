ba739cc041717a086bc031baf16faa9f3d8cfb62
Mark Metcalfe <mark.ls.metcalfe@gmail.com>
2022-02-28 14:30:07 +1300

    Fix transition animations

--- src/main.ts
+++ src/main.ts
@@ -17,5 +17,10 @@ router.beforeEach((to, _, next) => {
   }
   next()
 })
+router.afterEach((to, from) => {
+  const toDepth = to.path.match(/\/[^/]+?/g)?.length ?? 0
+  const fromDepth = from.path.match(/\/[^/]+?/g)?.length ?? 0
+  to.meta.transition = fromDepth > toDepth ? 'slide-left' : 'slide-right'
+})
 
 createApp(App).use(router).mount('#app')
