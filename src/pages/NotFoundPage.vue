002e40b52176612f3f42bb846b8a8afbee6c437c
Mark Metcalfe <mark.ls.metcalfe@gmail.com>
2022-02-28 16:55:00 +1300

    Add error pages

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <PageCard back-button-page="/">
+    <template #title>Not Found</template>
+    <p>The page you're looking for isn't here 😞</p>
+    <p>
+      Please <router-link to="/contact">contact me</router-link> if you think
+      this is an error
+    </p>
+  </PageCard>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import PageCard from '../components/PageCard.vue'
+export default defineComponent({
+  name: 'NotFoundPage',
+  components: { PageCard },
+})
+</script>
