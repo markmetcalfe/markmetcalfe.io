d8f0a048383dcb96634de97f45b681ef9b23bdd7
Mark Metcalfe <mark.ls.metcalfe@gmail.com>
2023-09-19 17:19:45 +1200

    Increase number of shapes based on mouse clicks

--- src/3d/main.ts
+++ src/3d/main.ts
@@ -7,31 +7,52 @@ export function initRenderer(container: HTMLElement): void {
   const scene = new THREE.Scene()
   const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
 
+  camera.position.z = 5
+
   const renderer = new THREE.WebGLRenderer()
   renderer.setSize(width, height)
 
-  const geometry = new THREE.OctahedronGeometry(1)
-  const geo = new THREE.EdgesGeometry(geometry)
-  const mat = new THREE.LineBasicMaterial({ color: 0x00ff00, linewidth: 2 })
-  const cube = new THREE.LineSegments(geo, mat)
+  const cubes: THREE.LineSegments[] = []
+  const numOfCubes = 3
+  for (let i = 0; i < numOfCubes; i++) {
+    const geometry = new THREE.OctahedronGeometry(1)
+    const geo = new THREE.EdgesGeometry(geometry)
+    const mat = new THREE.LineBasicMaterial({ color: 0x00ff00, linewidth: 2 })
+    const cube = new THREE.LineSegments(geo, mat)
+    scene.add(cube)
+    cubes.push(cube)
+  }
 
-  scene.add(cube)
-
-  camera.position.z = 5
+  let mouseClicks = 0
+  document.addEventListener('click', () => {
+    if (mouseClicks >= numOfCubes) {
+      mouseClicks = 0
+    } else {
+      mouseClicks++
+    }
+  })
 
   container.appendChild(renderer.domElement)
 
   function animate() {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
-    if (cube.position.z > 1) {
-      cube.position.z = 0
-    } else {
-      cube.position.z += 0.2
-    }
-    cube.rotation.x += 0.05
-    cube.rotation.y += 0.05
-    console.log(cube.position)
+    cubes.forEach((cube, index) => {
+      cube.visible = mouseClicks >= index
+
+      if (!cube.visible) {
+        return
+      }
+
+      if (cube.position.z > 1) {
+        cube.position.z = 0
+      } else {
+        cube.position.z += 0.2
+      }
+
+      cube.rotation.x += 0.05
+      cube.rotation.y += 0.05
+    })
   }
   animate()
 }
